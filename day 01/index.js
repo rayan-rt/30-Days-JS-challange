9e3f9d734706ec5cedd85a276e50a4c53a8d46d5
Rayan <tanzeemrayan2@gmail.com>
2024-07-13 12:23:26 +0500

    Day 01

---
+++ day 01/index.js
@@ -0,0 +1,53 @@
+console.clear();
+// Activity 01
+//  - Task 01
+var num_1 = 30;
+console.log(num_1);
+
+//  - Task 02
+let str_1 = "30 days JS challange";
+console.log(str_1);
+
+// Activity 02
+//  - Task 03
+const flag_1 = true;
+console.log(flag_1);
+
+// Activity 03
+//  - Task 04
+let num_2 = 30;
+console.log(typeof num_2);
+
+let str_2 = "String";
+console.log(typeof str_2);
+
+let flag_2 = true;
+console.log(typeof flag_2);
+
+let obj_1 = { key: "value" };
+console.log(typeof obj_1);
+
+let arr_1 = ["1"];
+console.log(typeof arr_1);
+
+// Activity 04
+//  - Task 05
+let rnd_1 = "initial";
+console.log(rnd_1);
+rnd_1 = "re assign";
+console.log(rnd_1);
+
+// Activity 05
+//  - Task 06
+const rnd_2 = "initial";
+console.log(rnd_2);
+// rnd_2 = "re assign"; // error
+// console.log(rnd_2);
+
+// Feature Request
+//  - Task 07
+let logs = [30, "string", true, { key: "value" }, ["1"]];
+logs.forEach((log) => console.log(`value: ${log} - type: ${typeof log}`));
+
+//  - Task 08
+// same as "- Task 06"
