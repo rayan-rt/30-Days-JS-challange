2314145bd0d8bd8eadf80e4ed7aa96a3ea5571b7
Rayan <tanzeemrayan2@gmail.com>
2024-07-14 10:39:39 +0500

    Day 02

---
+++ day 02/index.js
@@ -0,0 +1,49 @@
+console.clear();
+
+// Acitivity 01
+//  - Task 01 - 05
+let num_1 = 5;
+let num_2 = 10;
+console.log(num_1 + num_2);
+console.log(num_1 - num_2);
+console.log(num_1 * num_2);
+console.log(num_1 / num_2);
+console.log(num_1 % num_2);
+
+// Acitivity 02
+//  - Task 06 - 07
+num_1 += 5;
+console.log(num_1);
+num_1 -= 5;
+console.log(num_1);
+
+// Acitivity 03
+//  - Task 08 - 10
+console.log(num_1 < num_2);
+console.log(num_1 > num_2);
+
+console.log(num_1 <= num_2);
+console.log(num_1 >= num_2);
+
+console.log(num_1 == num_2);
+console.log(num_1 === num_2);
+
+// Acitivity 04
+//  - Task 11 - 13
+console.log(true && false);
+console.log(true || false);
+console.log(!(true && false));
+
+// Acitivity 05
+//  - Task 14
+console.log(num_1 > 0 ? "positive" : "negative");
+
+// Feature Request
+//  - Task 01 same as Activity 01
+
+//  - Task 02
+console.log(num_1 < num_2 && num_1 !== num_2);
+console.log(num_1 < num_2 || num_1 === num_2);
+console.log(!(num_1 < num_2 || num_1 === num_2));
+
+//  - Task 03 same as Activity 05
